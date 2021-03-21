c9af06f4286fc3d2959267052ec222e3e85132fb
Yuta Miyama <s04155yo@gmail.com>
2021-03-21 18:37:04 -0400

    Earn a dollar a second for 5 seconds

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,30 @@
 console.log("Hello");
+
+
+// Game state
+let account = 0;
+
+function earn() {
+    account += 1;
+    console.log(account);
+};
+
+const earningIntervalId = window.setInterval(earn, 1000);
+const stopEarning = () => window.clearInterval(earningIntervalId);
+window.setTimeout(stopEarning, 5000);
+
+
+// Render init
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.font = '50px serif';
+
+const renderer = {
+    render() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillText(`$ ${account}`, 70, 90);
+    },
+};
+
+window.setInterval(renderer.render.bind(renderer), 100);
