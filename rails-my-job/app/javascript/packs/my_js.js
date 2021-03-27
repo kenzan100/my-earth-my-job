d9e90eb9b9c7efc0fb6bba6bf21ce51e40a7cd9e
Yuta Miyama <s04155yo@gmail.com>
2021-03-27 18:48:19 -0400

    Express hourly rate

--- rails-my-job/app/javascript/packs/my_js.js
+++ rails-my-job/app/javascript/packs/my_js.js
@@ -4,18 +4,36 @@ console.log("client index.js");
 let account = 0;
 
 function earn() {
-    account += 1;
+    account += second_rate();
+};
+
+let beginner_job = {
+    hourly_rate: 11, // 11 USD
+    title: 'Beginner Job',
+};
+
+function second_rate() {
+    return beginner_job.hourly_rate / 3600;
 };
 
 const earningIntervalId = window.setInterval(earn, 1000);
+
 const stopEarning = () => window.clearInterval(earningIntervalId);
+// window.setTimeout(stopEarning, 5000);
+
 
 // Render init
-const canvas = document.getElementById('canvas');
+const money = document.getElementById('money');
+const title = document.getElementById('title');
 const renderer = {
+    init() {
+        title.innerText = beginner_job.title;
+    },
+
     render() {
-        canvas.innerText = `$ ${account}`;
+        money.innerText = `$ ${account.toFixed(2)}`;
     },
 };
 
+renderer.init();
 window.setInterval(renderer.render.bind(renderer), 100);
