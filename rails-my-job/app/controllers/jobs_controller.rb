1102d41ca68e2abd49e425f1d594dea3b9cb4534
Yuta Miyama <s04155yo@gmail.com>
2021-04-04 14:16:27 -0400

    Redis and foreman

--- rails-my-job/app/controllers/jobs_controller.rb
+++ rails-my-job/app/controllers/jobs_controller.rb
@@ -7,7 +7,7 @@ class JobsController < ApplicationController
     @supporting_attrs, @distracting_attrs = @job.job_attributes.partition do |attr|
       !attr.ditractor
     end
-    @success_rate_for_today = Person.yuta.success_rate(job: @job) + noise
+    @success_rate_for_today = Person.yuta.success_rate(job: @job) + noise(@job)
   end
 
   def create
@@ -19,7 +19,12 @@ class JobsController < ApplicationController
 
   private
 
-  def noise
-    rand(-10..10)
+  def noise(job)
+    Rails.cache.fetch(
+      "#{job.cache_key_with_version}/success_rate_noise",
+      expires_in: 24.hours,
+    ) do
+      rand(-10..10)
+    end
   end
 end
